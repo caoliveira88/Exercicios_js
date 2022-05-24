00d32faf44f693839c5b74eb65986a2a69ae4fd1
caoliveira88 <camilaoliver1@gmail.com>
2022-05-24 10:53:46 -0300

    Organização e sentenças

---
+++ Fundamentos/1-organizacao.js
@@ -0,0 +1,9 @@
+console.log("Sentença de código")// Senteças terminados com ponto e virgula ou não
+
+{
+    {
+        console.log("Olá")//bloco de código - agrupa sentença de códigos
+        console.log("Mundo!")
+    }
+    
+}
