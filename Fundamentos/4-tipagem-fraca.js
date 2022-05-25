a27c08ac0963f23c71144d373c35de01a62e9553
caoliveira88 <camilaoliver1@gmail.com>
2022-05-25 18:14:17 -0300

    Tipagem Fraca

---
+++ Fundamentos/4-tipagem-fraca.js
@@ -0,0 +1,18 @@
+let qualquer = "legal"
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+/*saída
+legal
+string
+3.1516
+number*/
+
+// Evitar nome genéricos e siglas - Exemplo 
+let valor = " "
+let numero = 1
+let pqp = false //Produto Químico Perigoso ¬¬
