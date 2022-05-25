6af818418b17c4fa74c6109b774287bd8b998f62
caoliveira88 <camilaoliver1@gmail.com>
2022-05-25 18:16:27 -0300

    Cuidados com Number

---
+++ Fundamentos/6-number-alguns-cuidados.js
@@ -0,0 +1,13 @@
+console.log(7 / 0)
+console.log("10" / 2)
+console.log("show!" * 2)
+console.log(0.1 + 0.7) 
+// console.log(10.toString()) não consegue chamar o litral . a função
+console.log((10.345).toFixed(2))//consegue chamar o literal dentro de parentes depois a função
+
+/*Saída
+Infinity
+5
+NaN - Not a Number
+0.7999999999999999
+10.35*/
