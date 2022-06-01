1ef2e6c4ca233382b4f853631fed249188b57fbe
caoliveira88 <camilaoliver1@gmail.com>
2022-06-01 17:49:07 -0300

    Hoisting

---
+++ Fundamentos/23-hoisting.js
@@ -0,0 +1,16 @@
+console.log("a = ", a)
+var a = 2
+console.log("a =", a)
+
+/*Saída
+  a =  undefined
+  a = 2*/
+
+// Com var acontece içamento, a variável é jogada pra cima
+
+
+console.log("a = ", a)
+let a = 2
+console.log("a =", a)
+
+// Com let não acontece, da erro
