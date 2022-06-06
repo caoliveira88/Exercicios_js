d262b540a5a39a71652002277bbe419529b22046
caoliveira88 <camilaoliver1@gmail.com>
2022-06-06 11:01:08 -0300

    Atribuicao

---
+++ Fundamentos/27-atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7 
+let b = 3
+
+b += a // Junto com a atribuicao estou acrescentando o valor de a na variável b / b = b + a
+console.log(b) // Saída - 10
+
+b -= 4 // Junto com a atribuicao subtrativa, estou subtraindo 4 do valor de b / b = b - 4
+console.log(b) // Saída - 6
+
+b *= 2 // Junto com a atribuicao multiplicativa, estou multiplicando o valor que está dentro de b / b recebe b multiplicado / b = b * 2
+console.log(b) // Saída - 12
+
+b /= 2 // Junto com a atribuicao divisiva, estou dividindo o valor que está dentro de b / b recebe b multiplicado / b = b / 2
+console.log(b) // Saída - 6
+
+b %= 2 // Junto com a atribuicao modular, valor do resto dentro de b / b = b % 2
+console.log(b) // Saída - 0
