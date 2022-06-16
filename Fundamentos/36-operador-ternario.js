fcea9ae5647a806c77ea681febebef6bb9a9ce0f
caoliveira88 <camilaoliver1@gmail.com>
2022-06-16 16:40:56 -0300

    Operador unário

---
+++ Fundamentos/36-operador-ternario.js
@@ -0,0 +1,8 @@
+// Exemplo operador ternário
+
+const resultado = nota => nota >=7 ? 'Aprovado' : 'Reprovado' // Operador ternario é composto por 3 partes Uma expressao/interrogacao/resultado se true/resultado se false
+
+console.log(resultado(7.1))
+console.log(resultado(6.7))
+
+const status = nota >= 7 ? 'Aprovado' : 'Reprovado' // Pode armazenar em uma constante
