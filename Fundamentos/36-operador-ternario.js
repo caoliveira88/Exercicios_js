6e4543d4c8f1dae49638cccdf8b9b5ceeba9149b
caoliveira88 <camilaoliver1@gmail.com>
2022-06-17 17:25:51 -0300

    Operador Ternário - Incluindo saída

--- Fundamentos/36-operador-ternario.js
+++ Fundamentos/36-operador-ternario.js
@@ -2,7 +2,7 @@
 
 const resultado = nota => nota >=7 ? 'Aprovado' : 'Reprovado' // Operador ternario é composto por 3 partes Uma expressao/interrogacao/resultado se true/resultado se false
 
-console.log(resultado(7.1))
-console.log(resultado(6.7))
+console.log(resultado(7.1)) // Saída - Aprovado
+console.log(resultado(6.7)) // Saída - Reprovado
 
 const status = nota >= 7 ? 'Aprovado' : 'Reprovado' // Pode armazenar em uma constante
